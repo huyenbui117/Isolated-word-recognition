d9da73f034f1b100f01bc56c8d4e87fb8c3d1377
unknown <huyenbui117@gmail.com>
2022-05-10 13:28:01 +0700

    feature extraction

--- common.py
+++ common.py
@@ -1,5 +1,7 @@
 import IPython
-LABELS = ["len", "xuong", "trai", "phai", "nhay", "ban", "A", "B", "sil"]
+LABELS = ["len", "xuong", "trai", "phai", "nhay", "ban", "A", "B"
+, "sil"
+]
 DATA_PATH = ['19020063_CaoDinhHoangMinh',
  '19021260_LeTruongGiang',
  '19021299_Nguyễn Đức Huy',
---
+++ feature_extraction.py
@@ -0,0 +1,79 @@
+import librosa
+import numpy as np
+import os
+import argparse
+import shutil
+import pickle
+from common import *
+import IPython
+def mfcc_extraction(file_path:str):
+    sound, sr = librosa.load(file_path)
+    mfcc = librosa.feature.mfcc(
+        y = sound, n_mfcc = 13, sr = sr, n_mels=128, fmax=8000, power=2,n_fft=1024
+    )
+    delta_mfcc = librosa.feature.delta(mfcc)
+    delta2_mfcc = librosa.feature.delta(mfcc, order = 2)
+    return np.concatenate((mfcc, delta_mfcc, delta2_mfcc))
+def feature_extraction(data_dir:str, output_path):
+    
+    for label in LABELS:
+        path = f"{data_dir}/{label}"
+        files = os.listdir(path)
+        features=[]
+        for file in files:
+            feature = mfcc_extraction(f"{path}/{file}")
+            features.append([feature,file[:-4]])
+        pickle.dump(features, open(f"{output_path}/{label}.sav", "wb"))
+    print(f"Extracted features from {data_dir}. Saved in {path}.sav")
+        
+def main():
+    LABELS.remove("sil")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_dir", type=str,
+        nargs = "?",
+        default = "data",
+        help="Path to dataset"
+    )
+    parser.add_argument(
+        "--multiple_dataset", type=bool,
+        nargs = "?",
+        default = False,
+        help =  "Set to true to extract multiple dataset in the same data_dir"
+    )
+    parser.add_argument(
+        "output_dir", type=str,
+        nargs = "?",
+        default = "feature",
+        help = "Path to folder containing extracted features"
+    )
+    parser.add_argument(
+        "--overwrite", type=bool,
+        nargs="?",
+        default=False,
+        help = "Overwrite the content of the extracted features directory. "
+    )
+
+    args = parser.parse_args()
+    if (os.path.exists(args.output_dir)):
+        if not args.overwrite:
+            raise ValueError(
+                f"Output directory ({args.output_dir}) is already existed and is not empty. Use --overwrite to overcome. "
+            )
+        else:
+            shutil.rmtree(args.output_dir)
+    os.mkdir(args.output_dir)
+
+    if args.multiple_dataset == True:
+        datasets = os.listdir(args.data_dir)
+        for dataset in datasets:
+            input_path = f"{args.data_dir}/{dataset}"
+            output_path = f"{args.output_dir}/{dataset}"
+            if not os.path.exists(output_path):
+                os.mkdir(output_path)
+            feature_extraction(input_path, output_path)
+    else:
+        feature_extraction(data_dir, output_dir)
+        
+if __name__ == '__main__':
+    main()
